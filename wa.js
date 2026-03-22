647422ea76b3649d53cec9f533c9ee0a2509cc23
deepanshushekhar2007-oss <deepanshushekhar2007@gmail.com>
2026-03-22 21:12:16 +0530

    Update wa.js

--- wa.js
+++ wa.js
@@ -1,19 +1,41 @@
 import pkg from "@whiskeysockets/baileys"
 const { default: makeWASocket, useMultiFileAuthState } = pkg
+
 import QRCode from "qrcode"
 import fs from "fs"
 
 async function start() {
+
     const { state, saveCreds } = await useMultiFileAuthState("auth")
 
-    const sock = makeWASocket({ auth: state })
+    const sock = makeWASocket({
+        auth: state,
+        printQRInTerminal: false
+    })
 
     sock.ev.on("creds.update", saveCreds)
 
-    sock.ev.on("connection.update", async ({ qr }) => {
+    sock.ev.on("connection.update", async (update) => {
+
+        const { connection, qr } = update
+
         if (qr) {
-            const img = await QRCode.toBuffer(qr)
-            fs.writeFileSync("qr.png", img)
+            console.log("QR RECEIVED")
+
+            try {
+                const buffer = await QRCode.toBuffer(qr)
+                fs.writeFileSync("qr.png", buffer)
+            } catch (e) {
+                console.log("QR error", e)
+            }
+        }
+
+        if (connection === "open") {
+            console.log("WhatsApp Connected")
+        }
+
+        if (connection === "close") {
+            console.log("Connection closed")
         }
     })
 }
